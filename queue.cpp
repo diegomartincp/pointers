70d06bd41f29274ce70bf60f545249b24f316a82
Diego martin <campos.martin.diego@gmail.com>
2020-10-01 19:10:56 +0200

    Añadimos eQ

--- queue.cpp
+++ queue.cpp
@@ -4,12 +4,22 @@
 
 #include <iostream>
 struct vox{
-    int v
-    vox *sgte;
+    int v;
+    vox *sgt;
 };
 
 vox *i; //i de in
 vox *f; //f de fin
+vox *aux; //creamos el auxiliar
+
+void eQ(int x){ //Donde x es la información que almacenamos en nuestra estructura tipo vox
+    //Preguntamos si la cola está llena o vacía
+    if(f==NULL){    //Si está vacía
+        vox *k = new vox;
+        k->v=x;
+        k->sgt;
+    }
+}
 
 int main{
 

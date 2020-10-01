6290224b015fe58d21229d6079cbd30311b9477e
Diego martin <campos.martin.diego@gmail.com>
2020-10-01 18:43:24 +0200

    Creamos el fichero

---
+++ queue.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Diego Martín Campos on 01/10/2020.
+//
+
+#include <iostream>
+struct vox{
+    int v
+    vox *sgte;
+};
+
+vox *i; //i de in
+vox *f; //f de fin
+
+int main{
+
+};

d56dc80bbb7ccfdb62a0ac98e65dc30e4c6c2b5a
diegomartinc <campos.martin.diego@gmail.com>
2020-10-06 17:25:35 +0200

    commit

---
+++ entregas_clase/5-10-2020 -Queue/cmake-build-debug/CMakeFiles/Queue5_10_2020.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Queue5_10_2020.dir/main.cpp.obj"
+  "Queue5_10_2020.exe"
+  "Queue5_10_2020.exe.manifest"
+  "Queue5_10_2020.pdb"
+  "libQueue5_10_2020.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Queue5_10_2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

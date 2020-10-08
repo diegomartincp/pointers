3e32431cd6e1218ac621ae39c804aeee753891e7
diegomartinc <campos.martin.diego@gmail.com>
2020-10-08 18:13:17 +0200

    añadimos *f

---
+++ codigo_clase/8-10-2020/cmake-build-debug/CMakeFiles/8_10_2020.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "8_10_2020.exe"
+  "8_10_2020.exe.manifest"
+  "8_10_2020.pdb"
+  "CMakeFiles/8_10_2020.dir/main.cpp.obj"
+  "lib8_10_2020.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_10_2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

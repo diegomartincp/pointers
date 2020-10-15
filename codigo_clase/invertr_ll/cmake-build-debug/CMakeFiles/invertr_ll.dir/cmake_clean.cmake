bbcdb771cf1181fe17474fca020f70d25833d62f
diegomartinc <campos.martin.diego@gmail.com>
2020-10-15 22:12:39 +0200

    Actualización de código

---
+++ codigo_clase/invertr_ll/cmake-build-debug/CMakeFiles/invertr_ll.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/invertr_ll.dir/main.cpp.obj"
+  "invertr_ll.exe"
+  "invertr_ll.exe.manifest"
+  "invertr_ll.pdb"
+  "libinvertr_ll.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/invertr_ll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

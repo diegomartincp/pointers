b2d0b8b9fe23088b4b8aead1144f0f875d91d70b
diegomartinc <campos.martin.diego@gmail.com>
2020-10-15 22:13:50 +0200

    actualización

--- codigo_clase/invertr_ll/main.cpp
+++ codigo_clase/invertr_ll/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+//Creamos una linked list para posteriormente invertirla con una pila
+
+struct nodo{
+    float v;
+    nodo *next;
+};
+nodo *head=NULL, *a;
+
+void mostrar(){
+    nodo *a=head;
+    while(a!=NULL){
+
+    }
+}
+void addPrincipio{
+        nodo *a=new nodo();
+
+};
+
+void addAntes{
+
+};
+
+void addDespues{
+
+};
+
 
 int main() {
+    cout << "Elegir acción" <<endl;
+
+
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
